013d4a56f393c40ea53b4d21f727ae62d33f8a29
reuben <reubenalabi2006@gmail.com>
2025-06-06 19:11:04 +0100

    config: Cors config update

--- src/config/cors-oprions.config.ts
+++ src/config/cors-oprions.config.ts
@@ -1,5 +1,9 @@
 export const corsOptions = {
-  origin: ['http://localhost:3000', 'http://localhost:5000'],
+  origin: [
+    'http://localhost:3000',
+    'http://localhost:5000',
+    'https://lux-net-product-4nqb.vercel.app/',
+  ],
   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
   credentials: true,
   allowedHeaders: [
